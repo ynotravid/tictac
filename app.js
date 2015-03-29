dfc5da43fa3e3b436cdbb3edf5c872b9dc8a50a2
ynotravid <tekasap@yahoo.com>
2015-03-29 02:26:45 -0700

    add initial files

---
+++ app.js
@@ -0,0 +1,38 @@
+var app = angular.module('flapperNews',[]);
+
+app.factory('posts', [function(){
+	var o = {
+		'posts': [
+			{'title': 'post 1', 'upvotes':5},
+			{'title': 'post 2', 'upvotes':2},
+			{'title': 'post 3', 'upvotes':15},
+			{'title': 'post 4', 'upvotes':9},
+			{'title': 'post 5', 'upvotes':4},
+		]
+
+	};
+	return o;
+}])
+.controller('MainCtrl', ['$scope', 'posts',
+	function($scope, posts){
+		$scope.title = 'Hello there';
+		$scope.posts = posts.posts;
+
+		$scope.addPost = function(post) {
+			if (!$scope.title) {return;}
+			$scope.posts.push({
+				title: $scope.title, 
+				link: $scope.link,
+				upvotes: 0
+			});
+			$scope.title = '';
+			$scope.link = '';
+		};
+
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes++;
+		};
+	}
+]);
+
+
