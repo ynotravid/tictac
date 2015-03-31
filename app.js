619e020c3cb1fe4841f676188429bce36ee10563
ynotravid <tekasap@yahoo.com>
2015-03-30 23:42:29 -0700

    Add a proper directory structure

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-var app = angular.module('flapperNews',[]);
+var app = angular.module('flapperNews',['ui.router']);
 
-app.factory('posts', [function(){
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+		.state(
+			'home',
+			{
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			}
+		).state(
+			'posts',
+			{
+				url:'/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			}
+		);
+		$urlRouterProvider.otherwise('home');
+	}
+])
+.factory('posts', [function(){
 	var o = {
 		'posts': [
 			{'title': 'post 1', 'upvotes':5},
@@ -27,12 +50,39 @@ app.factory('posts', [function(){
 			});
 			$scope.title = '';
 			$scope.link = '';
+
+$scope.posts.push({
+  title: $scope.title,
+  link: $scope.link,
+  upvotes: 0,
+  comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]
+});
+
+
 		};
 
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes++;
 		};
 	}
-]);
+])
+.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+
+$scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
+
+
+}]);
 
 
